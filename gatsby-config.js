95affe1f216eaccad4d3fa882b51df7cbd5cc246
Luis Diaz <luis@linuxmail.org>
2019-11-15 16:59:21 -0800

    Config for tailwind and purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,20 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-stripe`,
     {
+      resolve: 'gatsby-plugin-postcss',
+      options: {
+        postCssPlugins: [require("tailwindcss")],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-purgecss',
+      options: {
+        printRejected: false,
+        develop: false,
+        tailwind: true,
+      }
+    }
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
