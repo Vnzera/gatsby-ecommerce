aeeafc3f34bb3b8093e4f1c7c95de6bcd5800537
Luis Diaz <luis@linuxmail.org>
2019-11-22 18:22:49 -0800

    Navigation bar with hamburger menu for mobile

--- src/components/header.js
+++ src/components/header.js
@@ -3,20 +3,15 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
+  <header className="flex items-center justify-between bg-purple-800 px-4 py-3">
     <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
+    // style={{
+    //   margin: `0 auto`,
+    //   maxWidth: 960,
+    //   padding: `1.45rem 1.0875rem`,
+    // }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h2 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
@@ -26,9 +21,16 @@ const Header = ({ siteTitle }) => (
         >
           {siteTitle}
         </Link>
-      </h1>
+      </h2>
     </div>
-  </header>
+    <div>
+      <button type="button" className="text-gray-500 hover:text-white focus:text-white focus:outline-none">
+        <svg className="h-6 w-6 fill-current" viewbox="0 0 24 24">
+          <path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z" />
+        </svg>
+      </button>
+    </div>
+  </header >
 )
 
 Header.propTypes = {
