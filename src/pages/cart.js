262be5bd55a70a2bc726220a9c492210d8b189f1
Luis Diaz <luis@linuxmail.org>
2020-08-04 15:05:57 -0700

    addToCart function for storing user cart in localStorage

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -12,3 +12,30 @@ export default function Cart() {
         </Layout>
     )
 }
+// handleSubmit(sku) {
+  //   return event => {
+  //     event.preventDefault();
+
+  //     this.stripe
+  //       .redirectToCheckout({
+  //         // grab quantity from localStorage
+  //         items: [{ sku, quantity: 1 }],
+
+  //         // Do not rely on the redirect to the successUrl for fulfilling
+  //         // purchases, customers may not always reach the success_url after
+  //         // a successful payment.
+  //         // Instead use one of the strategies described in
+  //         // https://stripe.com/docs/payments/checkout/fulfillment
+  //         successUrl: 'https://ecommerce-gatsbyjs.netlify.com/success',
+  //         cancelUrl: 'https://ecommerce-gatsbyjs.netlify.com/canceled',
+  //       })
+  //       .then(function (result) {
+  //         if (result.error) {
+  //           // If `redirectToCheckout` fails due to a browser or network
+  //           // error, display the localized error message to your customer.
+  //           var displayError = document.getElementById('error-message');
+  //           displayError.textContent = result.error.message;
+  //         }
+  //       });
+  //   }
+  // }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,21 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { StaticQuery, graphql } from 'gatsby';
 
 // .redirectToCheckout logic will be moved to the cart page
 
-
-// function Product (props) {
-//   return (
-//     <div>
-
-//     </div>
-//   )
-// }
-
-
 class Product extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      count: 0
+      quantity: props.quantity,
+      id: props.id,
+      currency: props.currency,
+      price: props.price,
+      image: props.image,
+      name: props.name
     };
   }
 
@@ -30,55 +25,45 @@ class Product extends React.Component {
 
   // we want to add a clickable + and - so that users can update quantity
   // The Add To Cart button will add the selected quantity to the cart/localStorage
-  // "In Cart" text will appear next to item if added
+  // "In Cart" text will appear and buttons will be disabled 
 
   increment = () => {
     this.setState({
-      count: this.state.count + 1
+      quantity: this.state.quantity + 1
     });
   };
 
   decrement = () => {
-    if (this.state.count === 0) {
+    if (this.state.quantity === 0) {
       return;
     }
 
     this.setState({
-      count: this.state.count - 1
+      quantity: this.state.quantity - 1
     });
   };
 
-  // handleSubmit(sku) {
-  //   return event => {
-  //     event.preventDefault();
+  addToCart = () => {
+    // if localStorage is empty then add the item and return
+    let str = localStorage.getItem('cart');
 
-  //     this.stripe
-  //       .redirectToCheckout({
-  //         // grab quantity from localStorage
-  //         items: [{ sku, quantity: 1 }],
+    if (!str) {
+      localStorage.setItem('cart', JSON.stringify([this.state]));
+      return;
+    }
 
-  //         // Do not rely on the redirect to the successUrl for fulfilling
-  //         // purchases, customers may not always reach the success_url after
-  //         // a successful payment.
-  //         // Instead use one of the strategies described in
-  //         // https://stripe.com/docs/payments/checkout/fulfillment
-  //         successUrl: 'https://ecommerce-gatsbyjs.netlify.com/success',
-  //         cancelUrl: 'https://ecommerce-gatsbyjs.netlify.com/canceled',
-  //       })
-  //       .then(function (result) {
-  //         if (result.error) {
-  //           // If `redirectToCheckout` fails due to a browser or network
-  //           // error, display the localized error message to your customer.
-  //           var displayError = document.getElementById('error-message');
-  //           displayError.textContent = result.error.message;
-  //         }
-  //       });
-  //   }
-  // }
+    // parse localStorage into an array and push the current item to the cart
+
+    let arr = JSON.parse(str);
+    arr.push(this.state);
+    localStorage.setItem('cart', JSON.stringify(arr));
+
+    console.log("final check: ", localStorage.getItem('cart'));
+  }
 
   render() {
     // this has to do with formatting the price info we receive from stripe
-    const { id, currency, price, image, name } = this.props;
+    const { currency, price, image, name } = this.props;
 
     const priceFloat = (price / 100).toFixed(2);
     const formattedPrice = new Intl.NumberFormat('en-US', { style: 'currency', currency, maximumSignificantDigits: 2 }).format(priceFloat);
@@ -90,13 +75,14 @@ class Product extends React.Component {
         <img className="w-32 h-32 rounded object-cover" alt="product" src={image} />
         <p className="text-center"> {name} - {formattedPrice} </p>
 
+
         <button onClick={this.increment} className="m-1 bg-teal-600 hover:bg-teal-700 text-white font-bold py-1 px-4 rounded">+</button>
 
-        {this.state.count}
+        {this.state.quantity}
 
         <button onClick={this.decrement} className="m-1 bg-teal-600 hover:bg-teal-700 text-white font-bold py-1 px-4 rounded">-</button>
 
-        <button className="m-2 bg-teal-600 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded">Add to Cart</button>
+        <button onClick={this.addToCart} className="block m-2 bg-teal-600 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded">Add to Cart</button>
       </div>
     )
   }
@@ -130,11 +116,13 @@ const IndexPage = () => (
         <div className="flex flex-row flex-wrap flex-auto justify-center mx-auto my-20">
           {data.allStripeSku.edges.map(({ node: sku }) =>
             <Product
+              key={sku.id}
               id={sku.id}
               currency={sku.currency}
               price={sku.price}
               image={sku.image}
               name={sku.attributes.name}
+              quantity={0}
             />
           )}
         </div>
